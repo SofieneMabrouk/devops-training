79b0ce3eb32500fc165fb6425bfb7b78d0dd1ff6
SofieneMabrouk <sofienembk@gmail.com>
2026-02-17 13:42:13 +0100

    ci(api): replace paths-filter action with native path filtering

--- components/api/src/server.ts
+++ components/api/src/server.ts
@@ -4,6 +4,7 @@ import { consumeCoreAnswers } from "./queue/consumer";
 const PORT = process.env.PORT || 3000;
 
 // Start server
+//
 app.listen(PORT, async () => {
   console.log(`🚀 Express server running at http://localhost:${PORT}`);
 
